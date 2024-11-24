24a13164d85c4f9d4f2b98207afec2aa362704db
vinsdesign <kelvinsaputra024@gmail.com>
2024-11-24 12:28:34 +0800

    feat: detail members

--- members/views.py
+++ members/views.py
@@ -10,3 +10,12 @@ def members(request):
         "mymembers": mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+
+def details(request, id):
+    mymembers = Member.objects.get(id=id)
+    template = loader.get_template("details.html")
+    context = {
+        "mymembers": mymembers,
+    }
+    return HttpResponse(template.render(context, request))
