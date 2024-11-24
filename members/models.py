28380e7988d5475112f6d215ef64fe4863262ef5
vinsdesign <kelvinsaputra024@gmail.com>
2024-11-24 11:04:18 +0800

    feat: make migrations table members

--- members/models.py
+++ members/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone = models.IntegerField()
-    joined_date = models.DateField()
+    phone = models.IntegerField(null=True)
+    joined_date = models.DateField(null=True)
